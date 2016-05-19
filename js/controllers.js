713f50a07643dc383fde861b2b2b69a4db5fcc47
Ohad Yarel <ohadyarel@gmail.com>
2016-05-19 15:54:11 -0400

    adding toggle between the play and pause buttons

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	$('#play').click(function() {
 		jb.playJB();
+		$(this).hide();
+		$('#pause').show();
 	})
 	$('#pause').click(function() {
 		jb.pauseJB();
+		$(this).hide();
+		$('#play').show();
 	})
 	$('#next').click(function() {
 		playNext();
